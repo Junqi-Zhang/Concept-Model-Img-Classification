7a0b063310535110a5a255afd337e84e5dcd0011
Junqi Zhang <junqi.zhang.1012@gmail.com>
2023-08-29 13:58:36 +0800

    实现sparsemax版本模型

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.models import resnet18
 from collections import OrderedDict
+from sparsemax import Sparsemax
 
 
 class ResNet18(nn.Module):
@@ -327,6 +328,102 @@ class BasicConceptQuantizationV2(nn.Module):
         }
 
 
+class BasicConceptQuantizationV3(nn.Module):
+    def __init__(self, input_dim, num_classes, num_concepts, norm_concepts, norm_summary):
+        super(BasicConceptQuantizationV3, self).__init__()
+
+        self.input_dim = input_dim
+        self.norm_concepts = norm_concepts
+        self.norm_summary = norm_summary
+
+        # The shape of self.concepts should be C * D,
+        # where C represents the num_concepts,
+        # D represents the input_dim.
+        self.concepts = nn.Parameter(
+            torch.Tensor(num_concepts, input_dim)
+        )  # C * D
+
+        # W_q 和 W_k 设置为可学习参数
+        self.query_transform = nn.Parameter(
+            torch.Tensor(input_dim, input_dim)
+        )  # D * D
+        self.key_transform = nn.Parameter(
+            torch.Tensor(input_dim, input_dim)
+        )  # D * D
+
+        # 设置 W_v 为单位阵
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.value_transform = torch.eye(
+            input_dim,
+            dtype=torch.float,
+            device=device
+        )  # D * D
+
+        # 参数初始化
+        self.init_parameters()
+
+        # attention_weight sparsemax
+        self.sparsemax = Sparsemax(dim=1)
+
+        # 分类层
+        self.fc = nn.Linear(input_dim, num_classes)
+
+    def init_parameters(self):
+        # 初始化 concepts
+        nn.init.xavier_uniform_(self.concepts)
+        # 初始化 W_q 和 W_k
+        nn.init.xavier_uniform_(self.query_transform)
+        nn.init.xavier_uniform_(self.key_transform)
+
+    def forward(self, x):
+
+        # The shape of x should be B * D,
+        # where B represents the batch_size.
+
+        if self.norm_concepts:
+            concepts = torch.div(
+                self.concepts,
+                torch.norm(self.concepts, dim=1, p=2).view(-1, 1)
+            )
+        else:
+            concepts = self.concepts
+
+        # query, key, value 的线性变换
+        query = torch.matmul(x, self.query_transform)  # B * D
+        key = torch.matmul(concepts, self.key_transform)  # C * D
+        value = torch.matmul(concepts, self.value_transform)  # C * D
+
+        attention_weights = torch.matmul(query, key.t())  # B * C
+        attention_weights = attention_weights / \
+            torch.sqrt(torch.tensor(self.input_dim).float())
+        attention_weights = self.sparsemax(attention_weights)  # 尝试sparsemax
+
+        concept_summary = torch.matmul(attention_weights, value)  # B * D
+        if self.norm_summary:
+            # 按L2范数对concept_summary进行归一化
+            concept_summary = torch.div(
+                concept_summary,
+                torch.norm(concept_summary, dim=1, p=2).view(-1, 1)
+            )
+
+        # The shape of output is B * K,
+        # where K represents num_classes.
+        outputs = self.fc(concept_summary)
+
+        # 计算 concepts 的 cosine 相似度矩阵
+        concept_similarity = F.cosine_similarity(
+            concepts.unsqueeze(1),
+            concepts.unsqueeze(0),
+            dim=2
+        )  # C * C
+
+        return {
+            "outputs": outputs,
+            "attention_weights": attention_weights,
+            "concept_similarity": concept_similarity
+        }
+
+
 class BasicQuantResNet18(nn.Module):
     def __init__(self, num_classes, num_concepts, norm_concepts, norm_summary):
         super(BasicQuantResNet18, self).__init__()
@@ -390,11 +487,33 @@ class BasicQuantResNet18V2(nn.Module):
         return self.cq(x)
 
 
+class BasicQuantResNet18V3(nn.Module):
+    def __init__(self, num_classes, num_concepts, norm_concepts, norm_summary):
+        super(BasicQuantResNet18V3, self).__init__()
+
+        img_classifier = resnet18(weights=None, num_classes=num_classes)
+        self.backbone = nn.Sequential(*list(img_classifier.children())[:-1])
+
+        self.cq = BasicConceptQuantizationV3(
+            input_dim=512,
+            num_classes=num_classes,
+            num_concepts=num_concepts,
+            norm_concepts=norm_concepts,
+            norm_summary=norm_summary
+        )
+
+    def forward(self, x):
+        x = self.backbone(x)
+        x = x.view(x.size(0), -1)  # 512维向量 for ResNet18
+        return self.cq(x)
+
+
 PROVIDED_MODELS = OrderedDict(
     {
         "ResNet18": ResNet18,
         "BasicQuantResNet18": BasicQuantResNet18,
         "BasicQuantResNet18V1": BasicQuantResNet18V1,
-        "BasicQuantResNet18V2": BasicQuantResNet18V2
+        "BasicQuantResNet18V2": BasicQuantResNet18V2,
+        "BasicQuantResNet18V3": BasicQuantResNet18V3
     }
 )

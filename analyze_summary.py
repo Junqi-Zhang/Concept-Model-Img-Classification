3580ba4bc1cd22af6a1c65992c405a853abb5ecb
Junqi Zhang <junqi.zhang.1012@gmail.com>
2023-09-01 09:43:44 +0800

    优化analyze_summary

--- analyze_summary.py
+++ analyze_summary.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-summary_path = "./logs/standard/Sampled_ImageNet/summary_in_20230830.log"
+summary_path = "./logs/standard/Sampled_ImageNet/summary_in_20230831.log"
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
@@ -9,7 +9,7 @@ pd.set_option('display.max_colwidth', None)
 
 summary_df = pd.read_json(summary_path, lines=True)
 
-summary_df[['model', 'num_concepts', 'norm_concepts', 'norm_summary',
-            'grad_factor', 'loss_sparsity_weight', 'loss_diversity_weight',
-            'best_val_acc', 'best_val_acc_major',
-            'best_val_acc_minor', 'best_epoch', 'best_checkpoint_path']]
+select_df = summary_df[summary_df["model"] == "BasicQuantResNet18V3"][['model', 'num_concepts', 'norm_concepts', 'norm_summary',
+                                                                       'grad_factor', 'loss_sparsity_weight', 'loss_diversity_weight',
+                                                                      'best_val_acc', 'best_val_acc_major',
+                                                                       'best_val_acc_minor', 'best_epoch', 'best_checkpoint_path']]

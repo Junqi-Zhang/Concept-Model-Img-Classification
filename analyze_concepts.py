f19e477d627fb8120de95c14d678a1fa1d3c78f5
Junqi Zhang <junqi.zhang.1012@gmail.com>
2023-09-07 19:15:40 +0800

    优化概念分析与可视化代码

--- analyze_concepts.py
+++ analyze_concepts.py
@@ -1,7 +1,10 @@
 import os
 import shutil
+import sys
+import math
 from tqdm import tqdm
 from PIL import Image
+from collections import OrderedDict
 
 import numpy as np  # 在 import torch 前
 import torch
@@ -11,7 +14,9 @@ from torchvision import transforms
 from torchvision.datasets import ImageFolder
 
 from data_folders import PROVIDED_DATA_FOLDERS
-from models import PROVIDED_MODELS
+from models import MODELS
+from models_exp import MODELS_EXP
+from utils import capped_lp_norm, orthogonality_l2_norm, PIController
 from utils import load
 
 
@@ -26,22 +31,26 @@ if torch.cuda.is_available():
 else:
     device = "cpu"
 
-use_data_folder = "Sampled_ImageNet"
+use_data_folder = "Sampled_ImageNet_200x1000_50x100_Seed_6"
+# use_data_folder = "Sampled_ImageNet"
 use_data_folder_info = PROVIDED_DATA_FOLDERS[use_data_folder]
 num_classes = use_data_folder_info["num_classes"]
 
-use_model = "BasicQuantResNet18V3"
+PROVIDED_MODELS = OrderedDict(**MODELS, **MODELS_EXP)
+use_model = "BasicQuantResNet18V4"
 num_concepts = 50
-norm_concepts = False
-norm_summary = False
-grad_factor = 50
+num_attended_concepts = 5
+norm_concepts = True
+norm_summary = True
+grad_factor = 1
 loss_sparsity_weight = 0.0
+loss_sparsity_adaptive = False
 loss_diversity_weight = 1.0
 
 batch_size = 125
 
-load_checkpoint_path = "./checkpoints/Sampled_ImageNet/BasicQuantResNet18V3/202308291624_on_gpu_5/best_epoch_128_0.0222_0.9939_3.2961_0.6021_2.6051_0.6686_6.0602_0.3360.pt"
-checkpoint_desc = "不单位化概念_无正交约束"
+load_checkpoint_path = "./checkpoints/Sampled_ImageNet_200x1000_50x100_Seed_6/BasicQuantResNet18V4/202309031117_on_gpu_0/best_epoch_178_0.9831_0.6207_0.7026_0.7062_0.2932_0.6136_5.0_9.2_6.2_10.1.pt"
+checkpoint_desc = "50概念中位数5"
 
 ##########################
 # Dataset and DataLoader
@@ -76,31 +85,44 @@ eval_transform = transforms.Compose(
 
 # Load the dataset from the directory
 train_dataset = ImageFolder(root=train_data, transform=train_transform)
+train_classes_idx = [idx for idx in train_dataset.class_to_idx.values()]
 eval_dataset = ImageFolder(root=eval_data, transform=eval_transform)
+eval_classes_idx = [idx for idx in eval_dataset.class_to_idx.values()]
 
 # major_val 和 minor_val 的类别是 train 和 val 的子集
 tmp_major_dataset = ImageFolder(root=eval_major_data)
 tmp_minor_dataset = ImageFolder(root=eval_minor_data)
 
+major_to_train_idx_transform = dict()
+for key, value in tmp_major_dataset.class_to_idx.items():
+    major_to_train_idx_transform[value] = train_dataset.class_to_idx[key]
+
 
 def major_to_train(target):
-    idx_transform = dict()
-    for key, value in tmp_major_dataset.class_to_idx.items():
-        idx_transform[value] = train_dataset.class_to_idx[key]
-    return idx_transform[target]
+    return major_to_train_idx_transform[target]
+
+
+minor_to_train_idx_transform = dict()
+for key, value in tmp_minor_dataset.class_to_idx.items():
+    minor_to_train_idx_transform[value] = train_dataset.class_to_idx[key]
 
 
 def minor_to_train(target):
-    idx_transform = dict()
-    for key, value in tmp_minor_dataset.class_to_idx.items():
-        idx_transform[value] = train_dataset.class_to_idx[key]
-    return idx_transform[target]
+    return minor_to_train_idx_transform[target]
 
 
 eval_major_dataset = ImageFolder(
-    root=eval_major_data, transform=eval_transform, target_transform=major_to_train)
+    root=eval_major_data, transform=eval_transform, target_transform=major_to_train
+)
+eval_major_classes_idx = [
+    major_to_train(idx) for idx in eval_major_dataset.class_to_idx.values()
+]
 eval_minor_dataset = ImageFolder(
-    root=eval_minor_data, transform=eval_transform, target_transform=minor_to_train)
+    root=eval_minor_data, transform=eval_transform, target_transform=minor_to_train
+)
+eval_minor_classes_idx = [
+    minor_to_train(idx) for idx in eval_minor_dataset.class_to_idx.values()
+]
 
 
 # Create DataLoader instances
@@ -137,9 +159,17 @@ model = PROVIDED_MODELS[use_model](num_classes,
 load(model, load_checkpoint_path)
 
 criterion = nn.CrossEntropyLoss()
+sparsity_controller = PIController(
+    kp=0.001, ki=0.00001,
+    target_metric=num_attended_concepts,
+    initial_weight=loss_sparsity_weight
+)
 
 
-def compute_loss(returned_dict, targets):
+def compute_loss(returned_dict, targets, train=False):
+
+    global loss_sparsity_weight
+
     outputs = returned_dict["outputs"]
 
     # 代码兼容 ResNet18 等常规模型
@@ -154,25 +184,18 @@ def compute_loss(returned_dict, targets):
             loss_sparsity + loss_diversity_weight * loss_diversity
         return loss, loss_classification, loss_sparsity, loss_diversity
 
-    def concept_sparsity_reg():
-        # 熵越小, 分布越不均匀
-        # clamp attention_weights, 避免log2后出现nan
-        return torch.mean(-torch.sum(attention_weights.clamp(min=1e-9) *
-                                     torch.log2(attention_weights.clamp(min=1e-9)), dim=1))
-
-    # 防止 concept 退化, concept 之间要近似正交
-    def concept_diversity_reg():
-        # ideal_similarity 可以调整, 单位阵的假设过强
-        ideal_similarity = torch.eye(
-            num_concepts,
-            dtype=torch.float,
-            device=device
-        )
-        return torch.norm(concept_similarity-ideal_similarity)
-
     loss_classification = criterion(outputs, targets)
-    loss_sparsity = concept_sparsity_reg()
-    loss_diversity = concept_diversity_reg()
+    loss_sparsity = capped_lp_norm(attention_weights)
+    loss_diversity = orthogonality_l2_norm(concept_similarity)
+
+    def compute_s50(attention_weights):
+        with torch.no_grad():
+            s50 = torch.sum(attention_weights > 0, dim=1).median().item()
+        return s50
+
+    if loss_sparsity_adaptive and train:  # 只有训练的时候才能PI控制
+        s50 = compute_s50(attention_weights)
+        loss_sparsity_weight = sparsity_controller.update(s50)
 
     loss = loss_classification + loss_sparsity_weight * \
         loss_sparsity + loss_diversity_weight * loss_diversity
@@ -185,7 +208,7 @@ def compute_loss(returned_dict, targets):
 ##########################
 
 
-def run_epoch(desc, model, dataloader):
+def run_epoch(desc, model, dataloader, classes_idx):
 
     model.eval()
 
@@ -193,8 +216,12 @@ def run_epoch(desc, model, dataloader):
         "loss": 0.0,
         "loss_classification": 0.0,
         "loss_sparsity": 0.0,
+        "loss_sparsity_weight": loss_sparsity_weight,
         "loss_diversity": 0.0,
-        "acc": 0.0
+        "acc": 0.0,
+        "acc_subset": 0.0,
+        "s50": -1.0,
+        "s90": -1.0
     }
 
     attention = []
@@ -205,7 +232,9 @@ def run_epoch(desc, model, dataloader):
         total=len(dataloader),
         desc=desc,
         postfix=dict,
-        mininterval=0.3,
+        mininterval=1,
+        file=sys.stdout,
+        dynamic_ncols=True
     ) as pbar:
         for data, targets in dataloader:
             # data process
@@ -215,7 +244,7 @@ def run_epoch(desc, model, dataloader):
             with torch.no_grad():
                 returned_dict = model(data)
                 loss, loss_classification, loss_sparsity, loss_diversity = compute_loss(
-                    returned_dict, targets
+                    returned_dict, targets, train=False
                 )
 
             if returned_dict.get("attention_weights", None) is not None:
@@ -226,62 +255,82 @@ def run_epoch(desc, model, dataloader):
 
             # display the metrics
             with torch.no_grad():
+
                 acc = (torch.argmax(returned_dict["outputs"].data,
                                     1) == targets).sum() / targets.size(0)
+
+                mask = torch.zeros_like(returned_dict["outputs"].data)
+                mask[:, classes_idx] = 1
+                acc_subset = (torch.argmax(returned_dict["outputs"].data * mask,
+                                           1) == targets).sum() / targets.size(0)
+
+                if returned_dict.get("attention_weights", None) is not None:
+                    c = torch.sum(
+                        (returned_dict.get("attention_weights").data - 1e-3) > 0,
+                        dim=1
+                    ).cpu().numpy()
+                    n_selected_50 = np.percentile(c, 50)
+                    n_selected_90 = np.percentile(c, 90)
+                else:
+                    n_selected_50 = -1
+                    n_selected_90 = -1
+
             metric_dict["loss"] = (metric_dict["loss"] *
                                    step + loss.item()) / (step + 1)
             metric_dict["loss_classification"] = (metric_dict["loss_classification"] *
                                                   step + loss_classification.item()) / (step + 1)
             metric_dict["loss_sparsity"] = (metric_dict["loss_sparsity"] *
                                             step + loss_sparsity.item()) / (step + 1)
+            metric_dict["loss_sparsity_weight"] = loss_sparsity_weight
             metric_dict["loss_diversity"] = (metric_dict["loss_diversity"] *
                                              step + loss_diversity.item()) / (step + 1)
             metric_dict["acc"] = (metric_dict["acc"] *
                                   step + acc.item()) / (step + 1)
+            metric_dict["acc_subset"] = (metric_dict["acc_subset"] *
+                                         step + acc_subset.item()) / (step + 1)
+            metric_dict["s50"] = (metric_dict["s50"] *
+                                  step + n_selected_50) / (step + 1)
+            metric_dict["s90"] = (metric_dict["s90"] *
+                                  step + n_selected_90) / (step + 1)
+
             pbar.set_postfix(
                 **{
                     "loss": metric_dict["loss"],
                     "loss_cls": metric_dict["loss_classification"],
                     "loss_sps": metric_dict["loss_sparsity"],
+                    "loss_sps_w": metric_dict["loss_sparsity_weight"],
                     "loss_dvs": metric_dict["loss_diversity"],
-                    "acc": metric_dict["acc"]
+                    "acc": metric_dict["acc"],
+                    "acc_sub": metric_dict["acc_subset"],
+                    "s50": metric_dict["s50"],
+                    "s90": metric_dict["s90"]
                 }
             )
             pbar.update(1)
 
             step += 1
 
-    if len(attention) > 0:
-        analyze_sparsity(attention)
-
     return attention, label
 
 
-def analyze_sparsity(attn):
-    baseline = np.mean(attn)
-    percentile_25 = np.percentile(attn, 25) / baseline
-    percentile_50 = np.percentile(attn, 50) / baseline
-    percentile_75 = np.percentile(attn, 75) / baseline
-    percentile_90 = np.percentile(attn, 90) / baseline
-    percentile_99 = np.percentile(attn, 99) / baseline
-    print('25: %.5f, 50: %.5f, 75: %.5f, 90: %.5f, 99: %.5f' % (
-        percentile_25, percentile_50, percentile_75, percentile_90, percentile_99))
-
-
 desc = f"Training"
-train_attention, train_label = run_epoch(desc, model, train_loader)
+train_attention, train_label = run_epoch(
+    desc, model, train_loader, train_classes_idx
+)
 
 desc = f"Evaluate"
-eval_attention, eval_label = run_epoch(desc, model, eval_loader)
+eval_attention, eval_label = run_epoch(
+    desc, model, eval_loader, eval_classes_idx
+)
 
 desc = f"MajorVal"
 eval_major_attention, eval_major_label = run_epoch(
-    desc, model, eval_major_loader
+    desc, model, eval_major_loader, eval_major_classes_idx
 )
 
 desc = f"MinorVal"
 eval_minor_attention, eval_minor_label = run_epoch(
-    desc, model, eval_minor_loader
+    desc, model, eval_minor_loader, eval_minor_classes_idx
 )
 
 
@@ -291,26 +340,43 @@ analyze_attention = np.concatenate(train_attention, axis=0)
 label2name_dict = {value: key for key,
                    value in train_dataset.class_to_idx.items()}
 
-label_attention_dict = dict()
+label_concept_count_dict = dict()
 
 for label, attention in zip(analyze_label, analyze_attention):
-    if label not in label_attention_dict.keys():
-        label_attention_dict[label] = []
-    label_attention_dict[label].append(attention)
+    if label not in label_concept_count_dict.keys():
+        label_concept_count_dict[label] = np.zeros_like(
+            attention, dtype=int
+        )
+    indices = np.where(attention > 0.0)
+    label_concept_count_dict[label][indices] += 1
 
 label_concept_dict = dict()
 
-for label, attention_list in label_attention_dict.items():
-    indices = np.where(np.mean(attention_list, axis=0) > 0.1)
-    label_concept_dict[label] = list(indices[0])
+for label, count in label_concept_count_dict.items():
+    concept_count_dict = {
+        i: val for i,
+        val in enumerate(count) if val > 0
+    }
+    label_concept_dict[label] = {
+        k: v for k, v in sorted(
+            concept_count_dict.items(),
+            key=lambda item: (-item[1], item[0])
+        )
+    }
 
 concept_label_dict = dict()
-for label, concept_list in label_concept_dict.items():
-    for concept in concept_list:
+for label, concept_count_dict in label_concept_dict.items():
+    for concept, count in concept_count_dict.items():
         if concept not in concept_label_dict.keys():
-            concept_label_dict[concept] = []
-        concept_label_dict[concept].append(label)
-        concept_label_dict[concept].sort()
+            concept_label_dict[concept] = dict()
+        concept_label_dict[concept][label] = count
+for concept, label_count_dict in concept_label_dict.items():
+    concept_label_dict[concept] = {
+        k: v for k, v in sorted(
+            label_count_dict.items(),
+            key=lambda item: (-item[1], item[0])
+        )
+    }
 
 sorted_dict_items = sorted(label_concept_dict.items(), key=lambda x: x[0])
 label_concept_dict = dict(sorted_dict_items)
@@ -340,23 +406,34 @@ concept_images = get_top_k_filenames(train_images, analyze_attention, m * m)
 
 
 def stitch_images(image_paths, m):
-    # 计算每个小图的大小
-    first_image = Image.open(image_paths[0])
-    img_width, img_height = first_image.size
+    # 获取所有图片的大小
+    sizes = [Image.open(p).size for p in image_paths]
 
-    # 创建一个新的大图，大小为 m * img_width x m * img_height
-    stitched_image = Image.new('RGB', (m * img_width, m * img_height))
+    # 计算平均长宽比
+    avg_ratio = sum(width / height for width, height in sizes) / len(sizes)
 
-    # 遍历图片路径，将每个图片拼接到大图上
-    for index, image_path in enumerate(image_paths):
-        img = Image.open(image_path)
+    # 计算目标宽度和高度
+    target_height = min(height for width, height in sizes)
+    target_width = int(target_height * avg_ratio)
 
+    # 调整每张图片的大小
+    images = [Image.open(p) for p in image_paths]
+    resized_images = [img.resize(
+        (target_width, target_height), Image.LANCZOS
+    ) for img in images]
+
+    # 计算大图的大小
+    n = math.ceil(len(image_paths) / m)
+    stitched_image = Image.new('RGB', (m * target_width, n * target_height))
+
+    # 遍历图片，将每个图片粘贴到大图上
+    for index, img in enumerate(resized_images):
         # 计算当前图片在大图中的位置
         row = index // m
         col = index % m
 
         # 将图片粘贴到大图上
-        stitched_image.paste(img, (col * img_width, row * img_height))
+        stitched_image.paste(img, (col * target_width, row * target_height))
 
     return stitched_image
 

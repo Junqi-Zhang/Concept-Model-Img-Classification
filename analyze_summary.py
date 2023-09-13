3160fdcbe38c72a7e3a81a8304fcac9f99529ac7
Junqi Zhang <junqi.zhang.1012@gmail.com>
2023-09-13 19:23:16 +0800

    修改analyze_summary

--- analyze_summary.py
+++ analyze_summary.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-summary_path = "./logs/standard/Sampled_ImageNet_200x1000_200x25_Seed_6/summary_in_20230910.log"
+summary_path = "./logs/standard/Sampled_ImageNet_200x1000_200x25_Seed_6/summary_of_V4Smooth.log"
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
@@ -11,26 +11,30 @@ summary_df = pd.read_json(summary_path, lines=True)
 
 select_df = summary_df[
     summary_df["model"].isin(
-        ["BasicQuantResNet18V4"]
-    ) & (
-        summary_df["warmup_model"] != ""
+        ["BasicQuantResNet18V4", "BasicQuantResNet18V4Smooth"]
     )
 ][
-    ['model', 'warmup_checkpoint_epoch', 'num_concepts', 'num_attended_concepts', 'norm_concepts',
-     'loss_sparsity_weight', 'loss_sparsity_adaptive', 'loss_diversity_weight', 'batch_size',
+    ['model', 'num_concepts', 'num_attended_concepts', 'norm_concepts',
+     'loss_sparsity_weight', 'loss_sparsity_adaptive', 'loss_diversity_weight',
      'best_epoch', 'best_train_acc', 'best_val_acc', 'best_val_acc_major', 'best_val_acc_subset_major',
      'best_val_acc_minor', 'best_val_acc_subset_minor', 'best_val_s50', 'best_val_s90', 'best_val_loss_dvs',
      'checkpoint_dir', 'detailed_log_path']
 ]
 
 
-select_df[
-    ['model', 'num_concepts', 'num_attended_concepts', 'norm_concepts', 'loss_sparsity_adaptive', 'loss_diversity_weight',
-     'best_train_acc', 'best_val_acc', 'best_val_acc_major', 'best_val_acc_subset_major',
+grouped_df = select_df[
+    ['model', 'num_concepts', 'num_attended_concepts', 'norm_concepts', 'loss_sparsity_weight', 'loss_diversity_weight',
+     'best_epoch', 'best_train_acc', 'best_val_acc', 'best_val_acc_major', 'best_val_acc_subset_major',
      'best_val_acc_minor', 'best_val_acc_subset_minor', 'best_val_s50', 'best_val_s90', 'best_val_loss_dvs']
 ].groupby(
-    ['model', 'num_concepts', 'num_attended_concepts', 'norm_concepts',
-     'loss_sparsity_adaptive', 'loss_diversity_weight']
-).mean().sort_values(
-    "best_val_acc_subset_minor", ascending=False
-).to_excel("ResNet18.xlsx")
+    ['loss_sparsity_weight', 'model', 'num_concepts',
+        'num_attended_concepts', 'norm_concepts', 'loss_diversity_weight']
+)
+
+pd.concat(
+    [
+        grouped_df.mean(),
+        grouped_df.size().to_frame("count")
+    ],
+    axis=1
+).reset_index()

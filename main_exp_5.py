f08bb2a9155e3c6f01fafe788a363e409e6c6099
Junqi Zhang <junqi.zhang.1012@gmail.com>
2023-09-18 20:42:11 +0800

    重新布置实验2-9。

--- main_exp_5.py
+++ main_exp_5.py
@@ -15,20 +15,20 @@ parser.add_argument("--gpus", default=[0], type=int, nargs="*")
 
 seed_task_elements = {
     "mode": "standard",
-    "dataset_name": "Sampled_ImageNet_500x1000_500x5_Seed_6",
+    "dataset_name": "Sampled_ImageNet_500x1000_200x5_Seed_6",
     # "warmup_model": "",
     # "warmup_checkpoint_path": "",
     "use_model": "BasicQuantResNet18V4Smooth",
-    "num_concepts": 1000,
+    "num_concepts": 500,
     "num_attended_concepts": 100,
     "norm_concepts": False,
     "norm_summary": True,
     "grad_factor": 1,
-    "att_smoothing": 0.1,
-    "loss_sparsity_weight": 0.01,
+    "att_smoothing": 0.2,
+    "loss_sparsity_weight": 0.0,
     "loss_sparsity_adaptive": False,
     "loss_diversity_weight": 1.0,
-    "supplementary_description": "Test Rebuilt V4Smooth on 500_500 dataset",
+    "supplementary_description": "Test Rebuilt V4Smooth on 500_200 dataset",
     "num_epochs": 1000,
     "warmup_epochs": 10,
     "batch_size": 125,
@@ -44,22 +44,26 @@ def generate_tasks(seed_task_elements, parallel, gpus):
 
     # task 1
     new_task_element = seed_task_elements.copy()
+    new_task_element["loss_sparsity_weight"] = 0.02
+    new_task_element["num_attended_concepts"] = 60
     tasks.append(new_task_element)
 
     # task 2
     new_task_element = seed_task_elements.copy()
-    new_task_element["att_smoothing"] = 0.3
+    new_task_element["loss_sparsity_weight"] = 0.02
+    new_task_element["num_attended_concepts"] = 150
     tasks.append(new_task_element)
 
     # # task 3
     # new_task_element = seed_task_elements.copy()
-    # new_task_element["att_smoothing"] = 0.05
+    # new_task_element["loss_sparsity_weight"] = 0.02
+    # new_task_element["num_attended_concepts"] = 120
     # tasks.append(new_task_element)
 
     # # task 4
     # new_task_element = seed_task_elements.copy()
-    # new_task_element["att_smoothing"] = 0.4
-    # new_task_element["num_concepts"] = 500
+    # new_task_element["loss_sparsity_weight"] = 0.02
+    # new_task_element["num_attended_concepts"] = 80
     # tasks.append(new_task_element)
 
     if parallel:

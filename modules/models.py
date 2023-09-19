bb283a2fafe7f3ca2da9217f24e6b4504ffae065
Junqi Zhang <junqi.zhang.1012@gmail.com>
2023-09-19 20:12:40 +0800

    修复显存过大的BUG。

--- modules/models.py
+++ modules/models.py
@@ -135,145 +135,6 @@ class ContrastiveResNet18(nn.Module):
         return self.clf(x)
 
 
-class BasicConceptQuantizationV3(nn.Module):
-    def __init__(
-        self,
-        input_dim: int,
-        num_classes: int,
-        num_concepts: int,
-        norm_concepts: bool,
-        norm_summary: bool,
-        grad_factor: float
-    ):
-        super(BasicConceptQuantizationV3, self).__init__()
-
-        self.input_dim = input_dim
-        self.grad_factor = grad_factor
-        self.norm_concepts = norm_concepts
-        self.norm_summary = norm_summary
-
-        # The shape of self.concepts should be C * D,
-        # where C represents the num_concepts,
-        # D represents the input_dim.
-        self.concepts = nn.Parameter(
-            torch.Tensor(num_concepts, input_dim)
-        )  # C * D
-
-        # Set W_q and W_k as learnable parameters
-        self.query_transform = nn.Parameter(
-            torch.Tensor(input_dim, input_dim)
-        )  # D * D
-        self.key_transform = nn.Parameter(
-            torch.Tensor(input_dim, input_dim)
-        )  # D * D
-
-        # Set W_v as the identity matrix
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.value_transform = torch.eye(
-            input_dim,
-            dtype=torch.float,
-            device=device
-        )  # D * D
-
-        # Parameter initialization
-        self.init_parameters()
-
-        # attention_weight sparsemax
-        self.sparsemax = Sparsemax(dim=1)
-
-        # Classification layer
-        self.fc = nn.Linear(input_dim, num_classes)
-
-    def init_parameters(self) -> None:
-        # Initialize concepts
-        nn.init.xavier_uniform_(self.concepts)
-        # Initialize W_q and W_k
-        nn.init.xavier_uniform_(self.query_transform)
-        nn.init.xavier_uniform_(self.key_transform)
-
-    def forward(self, x: Tensor) -> Dict[str, Tensor]:
-
-        # The shape of x should be B * D,
-        # where B represents the batch_size.
-
-        if self.norm_concepts:
-            concepts = torch.div(
-                self.concepts,
-                torch.norm(self.concepts, dim=1, p=2).view(-1, 1)
-            )
-        else:
-            concepts = self.concepts
-
-        # Linear transformation of query, key, and value
-        query = torch.matmul(x, self.query_transform)  # B * D
-        key = torch.matmul(concepts, self.key_transform)  # C * D
-        value = torch.matmul(concepts, self.value_transform)  # C * D
-
-        attention_weights = torch.matmul(query, key.t())  # B * C
-        attention_weights = attention_weights / \
-            torch.sqrt(torch.tensor(self.input_dim).float())
-        attention_weights = self.sparsemax(attention_weights)  # Try sparsemax
-
-        concept_summary = torch.matmul(
-            attention_weights * self.grad_factor, value
-        )  # B * D
-        if self.norm_summary:
-            # Normalize concept_summary by L2 norm
-            concept_summary = torch.div(
-                concept_summary,
-                torch.norm(concept_summary, dim=1, p=2).view(-1, 1)
-            )
-
-        # The shape of output is B * K,
-        # where K represents num_classes.
-        outputs = self.fc(concept_summary)
-
-        # Calculate the cosine similarity matrix of concepts
-        concept_similarity = F.cosine_similarity(
-            concepts.unsqueeze(1),
-            concepts.unsqueeze(0),
-            dim=2
-        )  # C * C
-
-        return {
-            "outputs": outputs,
-            "attention_weights": attention_weights,
-            "concept_similarity": concept_similarity
-        }
-
-
-class BasicQuantResNet18V3(nn.Module):
-    def __init__(
-        self,
-        num_classes: int,
-        num_concepts: int,
-        norm_concepts: bool,
-        norm_summary: bool,
-        grad_factor: float,
-        *args,
-        **kwargs
-    ):
-        super(BasicQuantResNet18V3, self).__init__()
-
-        # Initialize the image classifier with ResNet18 architecture
-        img_classifier = resnet18(weights=None, num_classes=num_classes)
-        self.backbone = nn.Sequential(*list(img_classifier.children())[:-1])
-
-        self.cq = BasicConceptQuantizationV3(
-            input_dim=512,
-            num_classes=num_classes,
-            num_concepts=num_concepts,
-            norm_concepts=norm_concepts,
-            norm_summary=norm_summary,
-            grad_factor=grad_factor
-        )
-
-    def forward(self, x: Tensor) -> Dict[str, Tensor]:
-        x = self.backbone(x)
-        x = x.view(x.size(0), -1)  # 512-dimensional vector for ResNet18
-        return self.cq(x)
-
-
 class BasicConceptQuantizationV4(nn.Module):
     def __init__(
         self,
@@ -400,10 +261,15 @@ class BasicConceptQuantizationV4(nn.Module):
         ) * logit_scale  # B * K
 
         # Calculate the cosine similarity matrix for concepts
-        concept_similarity = F.cosine_similarity(
-            concepts.unsqueeze(1),
-            concepts.unsqueeze(0),
-            dim=2
+        if self.norm_concepts:
+            normalized_concepts = concepts
+        else:
+            normalized_concepts = torch.div(
+                concepts,
+                torch.norm(concepts, dim=1, p=2).view(-1, 1)
+            )
+        concept_similarity = torch.matmul(
+            normalized_concepts, normalized_concepts.t()
         )  # C * C
 
         return {
@@ -633,10 +499,15 @@ class BasicConceptQuantizationV4Smooth(nn.Module):
         ) * logit_scale  # B * K
 
         # Calculate the cosine similarity matrix of concepts
-        concept_similarity = F.cosine_similarity(
-            concepts.unsqueeze(1),
-            concepts.unsqueeze(0),
-            dim=2
+        if self.norm_concepts:
+            normalized_concepts = concepts
+        else:
+            normalized_concepts = torch.div(
+                concepts,
+                torch.norm(concepts, dim=1, p=2).view(-1, 1)
+            )
+        concept_similarity = torch.matmul(
+            normalized_concepts, normalized_concepts.t()
         )  # C * C
 
         return {
@@ -682,7 +553,6 @@ MODELS = OrderedDict(
         "ResNet18": ResNet18,
         "ResNet50": ResNet50,
         "ContrastiveResNet18": ContrastiveResNet18,
-        "BasicQuantResNet18V3": BasicQuantResNet18V3,
         "BasicQuantResNet18V4": BasicQuantResNet18V4,
         "BasicQuantResNet50V4": BasicQuantResNet50V4,
         "BasicQuantResNet18V4Smooth": BasicQuantResNet18V4Smooth
--- modules/models_backup.py
+++ modules/models_backup.py
@@ -1,26 +1,29 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision.models import resnet18, resnet50
 from torch import Tensor
-from torchvision.models import resnet18
-from typing import Dict
+from typing import Callable, Dict
 from collections import OrderedDict
+from sparsemax import Sparsemax
 
 
-class BasicConceptQuantizationV4NoSparse(nn.Module):
+class BasicConceptQuantizationV3(nn.Module):
     def __init__(
         self,
         input_dim: int,
         num_classes: int,
         num_concepts: int,
         norm_concepts: bool,
+        norm_summary: bool,
         grad_factor: float
     ):
-        super(BasicConceptQuantizationV4NoSparse, self).__init__()
+        super(BasicConceptQuantizationV3, self).__init__()
 
         self.input_dim = input_dim
         self.grad_factor = grad_factor
         self.norm_concepts = norm_concepts
+        self.norm_summary = norm_summary
 
         # The shape of self.concepts should be C * D,
         # where C represents the num_concepts,
@@ -45,29 +48,14 @@ class BasicConceptQuantizationV4NoSparse(nn.Module):
             device=device
         )  # D * D
 
-        # Learnable mapping matrix from image space to image-text joint space
-        self.image_projection = nn.Parameter(
-            torch.Tensor(input_dim, input_dim)
-        )  # D * D
-
-        # Classification layer
-        self.clf = nn.Parameter(
-            torch.Tensor(num_classes, input_dim)
-        )  # K * D, K represents the num_classes
-
-        # Scaler
-        self.logit_scale = nn.Parameter(torch.tensor(0.0))
-
         # Parameter initialization
         self.init_parameters()
 
-        # Attention_weight sparsemax
-        # self.sparsemax = Sparsemax(dim=1)
+        # attention_weight sparsemax
+        self.sparsemax = Sparsemax(dim=1)
 
-        # Normalization layers
-        # self.pre_layernorm = nn.LayerNorm(input_dim)
-        # self.concept_layernorm = nn.LayerNorm(input_dim)
-        self.post_layernorm = nn.LayerNorm(input_dim)
+        # Classification layer
+        self.fc = nn.Linear(input_dim, num_classes)
 
     def init_parameters(self) -> None:
         # Initialize concepts
@@ -75,10 +63,6 @@ class BasicConceptQuantizationV4NoSparse(nn.Module):
         # Initialize W_q and W_k
         nn.init.xavier_uniform_(self.query_transform)
         nn.init.xavier_uniform_(self.key_transform)
-        # Initialize the mapping matrix from image space to image-text joint space
-        nn.init.xavier_uniform_(self.image_projection)
-        # Initialize clf
-        nn.init.xavier_uniform_(self.clf)
 
     def forward(self, x: Tensor) -> Dict[str, Tensor]:
 
@@ -93,7 +77,7 @@ class BasicConceptQuantizationV4NoSparse(nn.Module):
         else:
             concepts = self.concepts
 
-        # Linear transformation of query, key, value
+        # Linear transformation of query, key, and value
         query = torch.matmul(x, self.query_transform)  # B * D
         key = torch.matmul(concepts, self.key_transform)  # C * D
         value = torch.matmul(concepts, self.value_transform)  # C * D
@@ -101,37 +85,21 @@ class BasicConceptQuantizationV4NoSparse(nn.Module):
         attention_weights = torch.matmul(query, key.t())  # B * C
         attention_weights = attention_weights / \
             torch.sqrt(torch.tensor(self.input_dim).float())
-        # attention_weights = self.sparsemax(attention_weights)  # Use sparsemax
-        attention_weights = F.softmax(attention_weights, dim=1)  # Use softmax
+        attention_weights = self.sparsemax(attention_weights)  # Try sparsemax
 
         concept_summary = torch.matmul(
             attention_weights * self.grad_factor, value
         )  # B * D
-        concept_summary = self.post_layernorm(concept_summary)  # B * D
-
-        image_embeds = torch.matmul(
-            concept_summary, self.image_projection
-        )  # B * D
-        # Normalize image_embeds by L2 norm
-        image_embeds = torch.div(
-            image_embeds,
-            torch.norm(image_embeds, dim=1, p=2).view(-1, 1)
-        )  # B * D
-
-        # Add noise to classification weights
-        clf = self.clf + (torch.rand_like(self.clf) - 0.5) * 0.01
-        # Normalize clf by L2 norm
-        clf = torch.div(
-            clf,
-            torch.norm(clf, dim=1, p=2).view(-1, 1)
-        )  # K * D
+        if self.norm_summary:
+            # Normalize concept_summary by L2 norm
+            concept_summary = torch.div(
+                concept_summary,
+                torch.norm(concept_summary, dim=1, p=2).view(-1, 1)
+            )
 
         # The shape of output is B * K,
         # where K represents num_classes.
-        logit_scale = self.logit_scale.exp()
-        outputs = torch.matmul(
-            image_embeds, clf.t()
-        ) * logit_scale  # B * K
+        outputs = self.fc(concept_summary)
 
         # Calculate the cosine similarity matrix of concepts
         if self.norm_concepts:
@@ -152,26 +120,29 @@ class BasicConceptQuantizationV4NoSparse(nn.Module):
         }
 
 
-class BasicQuantResNet18V4NoSparse(nn.Module):
+class BasicQuantResNet18V3(nn.Module):
     def __init__(
         self,
         num_classes: int,
         num_concepts: int,
         norm_concepts: bool,
+        norm_summary: bool,
         grad_factor: float,
         *args,
-        **kwargs,
+        **kwargs
     ):
-        super(BasicQuantResNet18V4NoSparse, self).__init__()
+        super(BasicQuantResNet18V3, self).__init__()
 
+        # Initialize the image classifier with ResNet18 architecture
         img_classifier = resnet18(weights=None, num_classes=num_classes)
         self.backbone = nn.Sequential(*list(img_classifier.children())[:-1])
 
-        self.cq = BasicConceptQuantizationV4NoSparse(
+        self.cq = BasicConceptQuantizationV3(
             input_dim=512,
             num_classes=num_classes,
             num_concepts=num_concepts,
             norm_concepts=norm_concepts,
+            norm_summary=norm_summary,
             grad_factor=grad_factor
         )
 
@@ -181,8 +152,8 @@ class BasicQuantResNet18V4NoSparse(nn.Module):
         return self.cq(x)
 
 
-MODELS_EXP = OrderedDict(
+MODELS = OrderedDict(
     {
-        "BasicQuantResNet18V4NoSparse": BasicQuantResNet18V4NoSparse
+        "BasicQuantResNet18V3": BasicQuantResNet18V3
     }
 )

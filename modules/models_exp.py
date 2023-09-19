9313978da8a9fb6d7bd1061543b83d1e77e381ca
Junqi Zhang <junqi.zhang.1012@gmail.com>
2023-09-19 20:28:04 +0800

    分离出仅有概念化功能的模块

--- modules/models_exp.py
+++ modules/models_exp.py
@@ -181,8 +181,135 @@ class BasicQuantResNet18V4NoSparse(nn.Module):
         return self.cq(x)
 
 
+class SmoothConceptQuantization(nn.Module):
+    def __init__(
+        self,
+        input_dim: int,
+        num_concepts: int,
+        norm_concepts: bool,
+        smoothing: float
+    ):
+        super(SmoothConceptQuantization, self).__init__()
+
+        self.input_dim = input_dim
+        self.norm_concepts = norm_concepts
+        # Strength of sparsemax smoothing (consider linking sparsity regularization weight)
+        self.smoothing = smoothing
+
+        # The shape of self.concepts should be C * D,
+        # where C represents the num_concepts,
+        # D represents the input_dim.
+        self.concepts = nn.Parameter(
+            torch.Tensor(num_concepts, input_dim)
+        )  # C * D
+
+        # W_q and W_k are set as learnable parameters
+        self.query_transform = nn.Parameter(
+            torch.Tensor(input_dim, input_dim)
+        )  # D * D
+        self.key_transform = nn.Parameter(
+            torch.Tensor(input_dim, input_dim)
+        )  # D * D
+
+        # Set W_v as the identity matrix
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.value_transform = torch.eye(
+            input_dim,
+            dtype=torch.float,
+            device=device
+        )  # D * D
+
+        # Parameter initialization
+        self.init_parameters()
+
+        # Attention_weight sparsemax
+        self.sparsemax = Sparsemax(dim=1)
+
+    def init_parameters(self) -> None:
+        # Initialize concepts
+        nn.init.xavier_uniform_(self.concepts)
+        # Initialize W_q and W_k
+        nn.init.xavier_uniform_(self.query_transform)
+        nn.init.xavier_uniform_(self.key_transform)
+
+    def forward(self, x: Tensor) -> Dict[str, Tensor]:
+
+        # The shape of x should be B * D,
+        # where B represents the batch_size.
+
+        if self.norm_concepts:
+            concepts = torch.div(
+                self.concepts,
+                torch.norm(self.concepts, dim=1, p=2).view(-1, 1)
+            )
+        else:
+            concepts = self.concepts
+
+        # Linear transformation of query, key, value
+        query = torch.matmul(x, self.query_transform)  # B * D
+        key = torch.matmul(concepts, self.key_transform)  # C * D
+        value = torch.matmul(concepts, self.value_transform)  # C * D
+
+        attention_weights = torch.matmul(query, key.t())  # B * C
+        attention_weights = attention_weights / \
+            torch.sqrt(torch.tensor(self.input_dim).float())
+        attention_weights = self.sparsemax(attention_weights)  # Use sparsemax
+        # attention_weights = F.softmax(attention_weights, dim=1)  # Use softmax
+
+        def smooth_tensor_matrix(input_matrix: torch.Tensor, smoothing=0.1):
+            """
+            Smooth every row in a tensor matrix in PyTorch
+            """
+            assert 0 <= smoothing < 1
+            num_classes = input_matrix.size(1)
+            non_zero_mask = input_matrix > 0
+            num_nonzero_per_row = non_zero_mask.sum(dim=1, keepdim=True)
+            num_zero_per_row = num_classes - num_nonzero_per_row
+            smoothing_value_for_zeros = (
+                smoothing / num_zero_per_row
+            ).expand_as(input_matrix)
+            smoothing_value_for_non_zeros = (
+                smoothing / num_nonzero_per_row
+            ).expand_as(input_matrix)
+            smoothed_matrix = input_matrix.clone()
+            smoothed_matrix[non_zero_mask] -= smoothing_value_for_non_zeros[non_zero_mask]
+            smoothed_matrix[~non_zero_mask] += smoothing_value_for_zeros[~non_zero_mask]
+            return smoothed_matrix
+
+        # Different forward propagation structures for train and eval modes
+        if self.training:
+            attention_weights_applied = smooth_tensor_matrix(
+                attention_weights, self.smoothing
+            )
+        else:
+            attention_weights_applied = attention_weights
+
+        concept_summary = torch.matmul(
+            attention_weights_applied, value
+        )  # B * D
+
+        # Calculate the cosine similarity matrix of concepts
+        if self.norm_concepts:
+            normalized_concepts = concepts
+        else:
+            normalized_concepts = torch.div(
+                concepts,
+                torch.norm(concepts, dim=1, p=2).view(-1, 1)
+            )
+        concept_similarity = torch.matmul(
+            normalized_concepts, normalized_concepts.t()
+        )  # C * C
+
+        return {
+            "concept_summary": concept_summary,
+            "attention_weights": attention_weights,
+            "concept_similarity": concept_similarity
+        }
+
+
 MODELS_EXP = OrderedDict(
     {
-        "BasicQuantResNet18V4NoSparse": BasicQuantResNet18V4NoSparse
+        "BasicQuantResNet18V4NoSparse": BasicQuantResNet18V4NoSparse,
+        "SmoothConceptQuantization": SmoothConceptQuantization
     }
 )

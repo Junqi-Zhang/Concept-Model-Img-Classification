e69286a306bb998402c9988fa792499c8ef474db
Junqi Zhang <junqi.zhang.1012@gmail.com>
2023-09-23 21:51:58 +0800

    修改分析summary的代码

--- analyze_summary.py
+++ analyze_summary.py
@@ -11,22 +11,19 @@ summary_df = pd.read_json(summary_path, lines=True)
 
 select_df = summary_df[
     summary_df["use_model"].isin(
-        ["OriTextQuantResNet18", "OriTextResNet18", "OriTextSoftQuantResNet18", "OriTextResNet50"]
+        ["OriTextQuantResNet18", "OriTextResNet18", "OriTextSoftQuantResNet18",
+         "OriTextCQPoolResNet18"]
     )
-][
-    ['use_model', 'num_concepts', 'num_attended_concepts', 'norm_concepts', 'att_smoothing',
-     'loss_sparsity_weight', 'loss_sparsity_adaptive', 'loss_diversity_weight',
-     'best_epoch', 'best_train_acc', 'best_val_acc', 'best_major_acc', 'best_major_acc_subset', 'best_minor_acc', 'best_minor_acc_subset',
-     'best_train_s10', 'best_train_s50', 'best_train_s90',
-     'best_val_s10', 'best_val_s50', 'best_val_s90',
-     'best_major_s10', 'best_major_s50', 'best_major_s90',
-     'best_minor_s10', 'best_minor_s50', 'best_minor_s90',
-     'best_val_loss_dvs',
-     'checkpoint_dir', 'detailed_log_path', 'num_classes']
 ]
+# select_df[["concept_attn_head", "patch_attn_head"]] = select_df[[
+#     "concept_attn_head", "patch_attn_head"]].fillna("")
+# select_df[["concept_attn_max_fn", "patch_attn_max_fn"]] = select_df[[
+#     "concept_attn_max_fn", "patch_attn_max_fn"]].fillna("")
+select_df.fillna("", inplace=True)
 
 grouped_df = select_df[
     ['use_model', 'num_concepts', 'num_attended_concepts', 'norm_concepts', 'att_smoothing',
+     'concept_attn_head', 'concept_attn_max_fn', 'patch_attn_head', 'patch_attn_max_fn',
      'loss_sparsity_weight', 'loss_sparsity_adaptive', 'loss_diversity_weight',
      'best_epoch', 'best_train_acc', 'best_val_acc', 'best_major_acc', 'best_major_acc_subset', 'best_minor_acc', 'best_minor_acc_subset',
      'best_train_s10', 'best_train_s50', 'best_train_s90',
@@ -36,6 +33,7 @@ grouped_df = select_df[
      'best_val_loss_dvs', 'num_classes']
 ].groupby(
     ['num_classes', 'use_model', 'att_smoothing', 'num_concepts', 'loss_sparsity_weight', 'num_attended_concepts',
+     'concept_attn_head', 'concept_attn_max_fn', 'patch_attn_head', 'patch_attn_max_fn',
      'loss_sparsity_adaptive', 'norm_concepts', 'loss_diversity_weight']
 )
 
